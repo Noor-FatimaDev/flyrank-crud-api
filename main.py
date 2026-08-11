297d9c55ecb670d402d9e668a56a9e51555e0d84
Noor Fatima <bsf23005082@gmail.com>
2026-08-12 02:12:30 +0500

    Stage 1: added /health endpoint

--- main.py
+++ main.py
@@ -4,3 +4,7 @@ app = FastAPI()
 @app.get("/")
 def read_something():
     return {"message": "Hello, World!"}
+
+@app.get("/health")
+def check_health():
+    return {"name": "Task API", "version": "1.0", "endpoints": ["/health", "/tasks"]}
