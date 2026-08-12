9880cde565730c343518ee9d5213b9b85d8ad3a0
Noor Fatima <bsf23005082@gmail.com>
2026-08-12 18:45:55 +0500

    Stage 3: POST - tasks with validation and 201 status

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+
+class Create_task(BaseModel):
+    title: str
+    done: bool=False
+
+class Task(BaseModel):
+    id: str
+    title: str
+    done: bool=False
 
 app = FastAPI()
 
 tasks = [
-        {"id": "1", "title": "Task 1", "done": False},
-        {"id": "2", "title": "Task 2", "done": False},
-        {"id": "3", "title": "Task 3", "done": False}
+        Task(id="1", title="Task 1", done=False),
+        Task(id="2", title="Task 2", done=False),
+        Task(id="3", title="Task 3", done=False)
         ]
  
 @app.get("/")
@@ -24,6 +34,16 @@ def get_tasks():
 @app.get("/tasks/{id}")
 def get_task(id: str):
     for task in tasks:
-        if task["id"] == id:
+        if task.id == id:
             return task
-    return {"error": "Task not found"}
+    raise HTTPException(status_code=404, detail="Task not found")
+
+@app.post("/tasks", status_code=201)
+def create_task(task: Create_task):
+    if task.title == "":
+        raise HTTPException(status_code=400, detail="Title cannot be empty")
+    else:
+        new_task = Task(id=str(max(int(t.id) for t in tasks) + 1), title=task.title, done=task.done)
+        tasks.append(new_task)
+        return {"message": "Created ", "task": new_task}
+
